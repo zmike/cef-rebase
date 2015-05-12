842e2a5be6635db1799355f4c61649b7723d7d2e
Mike Blumenkrantz <zmike@osg.samsung.com>
2015-05-12 18:40:33 -0400

    add back browser->composite() method

--- libcef_dll/cpptoc/browser_host_cpptoc.cc
+++ libcef_dll/cpptoc/browser_host_cpptoc.cc
@@ -893,6 +893,17 @@ void CEF_CALLBACK browser_host_drag_source_system_drag_ended(
   CefBrowserHostCppToC::Get(self)->DragSourceSystemDragEnded();
 }  // namespace
 
+void CEF_CALLBACK browser_host_composite(struct _cef_browser_host_t* self) {
+  // AUTO-GENERATED CONTENT - DELETE THIS COMMENT BEFORE MODIFYING
+
+  DCHECK(self);
+  if (!self)
+    return;
+
+  // Execute
+  CefBrowserHostCppToC::Get(self)->Composite();
+}
+
 void CEF_CALLBACK browser_host_initialize_compositing(
     struct _cef_browser_host_t* self) {
   // AUTO-GENERATED CONTENT - DELETE THIS COMMENT BEFORE MODIFYING
@@ -965,6 +976,7 @@ CefBrowserHostCppToC::CefBrowserHostCppToC() {
   GetStruct()->drag_source_ended_at = browser_host_drag_source_ended_at;
   GetStruct()->drag_source_system_drag_ended =
       browser_host_drag_source_system_drag_ended;
+  struct_.struct_.composite = browser_host_composite;
   struct_.struct_.initialize_compositing = browser_host_initialize_compositing;
 }
 
--- libcef_dll/ctocpp/browser_host_ctocpp.h
+++ libcef_dll/ctocpp/browser_host_ctocpp.h
@@ -92,6 +92,7 @@ class CefBrowserHostCToCpp
   virtual void DragTargetDrop(const CefMouseEvent& event) OVERRIDE;
   virtual void DragSourceEndedAt(int x, int y, DragOperationsMask op) OVERRIDE;
   virtual void DragSourceSystemDragEnded() OVERRIDE;
+  virtual void Composite() OVERRIDE;
   virtual void InitializeCompositing() OVERRIDE;
 };
 
